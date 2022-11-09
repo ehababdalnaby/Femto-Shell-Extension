a2c1562e2450c99201df4739371365158b2313b9
Ehab Omara <89157664+ehababdalnaby@users.noreply.github.com>
2022-11-09 15:25:57 +0200

    make prompt length dynamic

--- prompt/prompt.c
+++ prompt/prompt.c
@@ -4,17 +4,24 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define PROMPT_SIZE 100
 char *home = NULL;
-static char prompt[PROMPT_SIZE] = {0};
- char *printPrompet(void) {
+//static char prompt[PROMPT_SIZE] = {0};
+
+
+char *printPrompet(void) {
+       static char *prompt = NULL;
+       prompt = (char*)calloc(1,100);
   char host[10] = {0};
   struct passwd *user = NULL;
   user = getpwuid(geteuid());
   home = user->pw_dir;
   gethostname(host, 10);
   char dir[PATH_MAX] = {0};
+  prompt = realloc(prompt,PATH_MAX+strlen(user->pw_name)+strlen(host)+1);
   sprintf(prompt,
           USER_COLOR "%s@%s" ANSI_COLOR_RESET ":" DIR_COLOR
                      "%s" ANSI_COLOR_RESET"$ ",
